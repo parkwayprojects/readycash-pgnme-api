dfd808d2efca8a5d8220f3f6f9b7ce8fec4d991f
jilesanmi <ilesanmi.josiah@gmail.com>
2020-06-12 18:32:00 +0100

    wip

--- utils/helpers.js
+++ utils/helpers.js
@@ -89,10 +89,21 @@ const sanefCtrl = (req, res) => {
 
     gpg.encrypt(message, args, function (err, encrypted) {
 
-      console.log("Encrypted Value",encrypted.toString())
+    //  console.log("Encrypted Value",encrypted.toString())
 
-     let encMessage = encrypted.toString();
+     //let encMessage = encrypted.toString();
 
+     const encMessage = `hIwDu9a+1IKS4RgBA/9FtKH1o5Yz77NR1h0H0N/eRcV7bVpxFmEZWkS9QV0hiK7+
+     W8k9tx/nOsaL8fWPB8k+E5EU50c5rGFYHxNQLh83q7FM5Ek6ACcRX9YwZAdFi+IT
+     hBtIUTsoePs7hGHE6wFo2BAY9O8rUDAl89kOS42ISjyQlwD0jJoyr2gRTWAQVNLA
+     fgGeD66FVbSpfTsZp2imhhDNRwCgyEMITtHuaB3dRdXEBzL/sJMZfdKQeZ5pzVel
+     ex1RffWvu0okzyl7skCSjsgY6ZPfXPN+JKLtPa+jUtbORvY7AvRYiSePxTc2SRHj
+     /gDYKHXbT8N1f/U4feb/UlOfdi7FNQABr4LA3I2go6h06tdPNwV0t6iWZTdlhwvS
+     vBc4sxAJeka0BotNwzFW0QL949b/wtqVEz6mGZsaByL3EAQEfXsR1jYgMdXkV86u
+     Gh+xHLu+5hxypMReSWyHpmVq+yTcx7iAA3St/oEQegWxwJ0SC8kkKXLtU9ntCKhS
+     IkskXnyDR8lybXXamvEs3USLNXu/6Wt+x/ItLxGl9siN0CyW+/89KkWMgdJ/BMvh
+     sWzOByYEnI3NiwCalOcctgymxCB2wxC6e9LfTuE9sA==
+     =50xS`;
      
       axios
         .post(
@@ -101,7 +112,10 @@ const sanefCtrl = (req, res) => {
           { headers: { 'ClientID': "018" } }
         )
         .then((data) => res.send(data))
-        .catch((error) => console.log(error))
+        .catch((error) => {
+          console.log(error)
+          res.end()
+        })
     });
   });
 };
