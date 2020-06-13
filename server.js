176df9c49d592f1e950c42ef1d1ae8d35eed4c48
jilesanmi <ilesanmi.josiah@gmail.com>
2020-06-13 19:14:55 +0100

    sanef decryption

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const {register, login} = require('./controllers/userController')
 const { registerValidation, loginValidation } = require('./validations/register')
 const { protect } = require('./utils/auth')
 const {catchAsync } = require('./handlers/errorHandler')
-const { sanefCtrl } = require('./utils/helpers')
+const { sanefCtrl, hashing, decrypt } = require('./utils/helpers')
 const errors = require('./handlers/errorHandler')
 require('dotenv').config({ path: 'variables.env'})
 
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}))
 
 
-app.get('/api/sanef', sanefCtrl)
+app.get('/api/sanef', hashing)
 
 
 app.post('/api/register',registerValidation, catchAsync(register))
